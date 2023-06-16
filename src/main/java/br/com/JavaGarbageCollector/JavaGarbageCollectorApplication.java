a4518e7f552526b35ecd6265ec24f4266c448d06
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-06-16 11:45:45 -0300

    Garbage Collector

--- src/main/java/br/com/JavaGarbageCollector/JavaGarbageCollectorApplication.java
+++ src/main/java/br/com/JavaGarbageCollector/JavaGarbageCollectorApplication.java
@@ -12,7 +12,35 @@ public class JavaGarbageCollectorApplication {
 		System.out.println("\n----------------------");
 		System.out.println("ENTROU NA APLICAÇÃO!!!");
 		System.out.println("----------------------\n");
-		
+
+		Usuario[] usuarios = new Usuario[10_000_000];
+
+		for (int i = 0; i < usuarios.length; i++) {
+			Usuario usuario = new Usuario(Long.valueOf(i), "usuario" + i, "usuarioMail" + i);
+			usuarios[i] = usuario;
+			// System.out.println(usuario.id());
+		}
+
+		System.out.println("Usuários criados!!!");
+		System.out.print("Memória utilizada: ");
+		obterMemoriaUsada();
+		System.out.println("MB");
+
+		usuarios = null;
+
+		Runtime.getRuntime().runFinalization();
+		Runtime.getRuntime().gc();
+
+		System.out.println("\nUsuários removidos da memória!!!");
+		System.out.print("Memória utilizada: ");
+		obterMemoriaUsada();
+		System.out.print("MB");
+	}
+
+	public static void obterMemoriaUsada() {
+		final int MB = 1024 * 1024;
+		Runtime runtime = Runtime.getRuntime();
+		System.out.print((runtime.totalMemory() - runtime.freeMemory()) / MB);
 	}
 
 }
---
+++ src/main/java/br/com/JavaGarbageCollector/Usuario.java
@@ -0,0 +1,5 @@
+package br.com.JavaGarbageCollector;
+
+public record Usuario(Long id, String nome, String email) {
+    
+}
